4407890f016906af9a85f688af21d56d8fb10ba7
yingchao <yingchao.usc2013@gmail.com>
2017-01-30 18:31:25 -0800

    add client code

---
+++ client/app/components/App.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var GuineaPigs = React.createClass({
+
+  render: function () {
+    var src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <p> hahahaha </p>
+        <a href="http://127.0.0.1:8000/users"> test </a>
+        <p> abcdedf </p>
+      </div>
+    );
+  }
+});
+
+module.exports = GuineaPigs;
